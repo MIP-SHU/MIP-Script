41d9843c2e2a9c8744695294f919a0e58cfd2cba
zuston.shacha <zuston.shacha@gmail.com>
2017-09-27 00:07:13 +0800

    nova宕机微信通知

--- supervisor.py
+++ supervisor.py
@@ -5,7 +5,7 @@ import time
 # 自信满满的等待接收即可
 while 1 :
     try:
-        req.get("http://http://localhost:8080/m/s?param=sdsdsd",timeout=10000)
+        req.get("http://nova.shu.edu.cn:7000/MIP-FrontEnd/main.html?token=123456789",timeout=10000)
         time.sleep(60)
     except Exception, e :
         req.get("https://pushbear.ftqq.com/sub",params={"sendkey":"981-22aabe24d9c47d339102df259f85fa08","text":"NovaStatus","desp":"Nova离线中"})
